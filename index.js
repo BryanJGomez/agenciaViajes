a50c96d84c11eae538d72d53594477af43f0ea55
bryan gomez <bryangomez665@gmail.com>
2020-12-30 11:44:32 -0600

    agregando pug

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import  express from 'express';
+import router from './routes/index.js';
 
 
 const app = express();
 
 const port = process.env.PORT || 4000;
 
+//habilitar PUG
+app.set('view engine', 'pug');
+
+//agregamos router
+app.use('/', router)
 
 app.listen(port, ()=>{
     console.log(`El servidor esta funcionando en el puerto ${port}`);
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,20 @@
 import  express from 'express';
 
-const route = express.Router();
+const router = express.Router();
 
-route.get('/', (req, res)=>{ // req lo que estamos enviando || res lo que express nos responde
+router.get('/', (req, res)=>{ // req lo que estamos enviando || res lo que express nos responde
     res.send ('Inicio');
      
 });
 
-route.get('/nosotros', (req, res)=>{ // req lo que estamos enviando || res lo que express nos responde
+router.get('/nosotros', (req, res)=>{ // req lo que estamos enviando || res lo que express nos responde
     res.send ('Nosotros');
      
 });
 
-route.get('/contacto', (req, res)=>{ // req lo que estamos enviando || res lo que express nos responde
+router.get('/contactos', (req, res)=>{ // req lo que estamos enviando || res lo que express nos responde
     res.send ('Contacto');
      
 });
+
+export default router;
