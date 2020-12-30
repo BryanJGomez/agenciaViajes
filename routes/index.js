74935a2354c36852fe830d3ebdc047849cefce25
bryan gomez <bryangomez665@gmail.com>
2020-12-30 12:25:30 -0600

    agregando hoja de estilos y imagenes

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ import  express from 'express';
 
 const router = express.Router();
 
-router.get('/inicio', (req, res)=>{ // req lo que estamos enviando || res lo que express nos responde
-    res.send ('Inicio');
+router.get('/', (req, res)=>{ // req lo que estamos enviando || res lo que express nos responde
+    res.render('inicio');
      
 });
 
